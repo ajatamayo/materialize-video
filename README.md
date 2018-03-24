02bc3af70fcefe2d2bd2b68f91ca97aabccefde8
aj <aj@icannhas.com>
2018-03-24 11:07:21 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# materialize-video
+Wordpress Plugin: Display videos as Materialize Cards. Captions will be shown as card contents if specified.
+
+Cards are a convenient means of displaying content composed of different types of objects. They’re also well-suited for presenting similar objects whose size or supported actions can vary considerably, like photos with captions of variable length.
+
+When the `video` shortcode is used, the video will be displayed as a Materialize Card Image as specified here http://next.materializecss.com/cards.html.
---
+++ materialize-video.php
@@ -0,0 +1,113 @@
+<?php
+/**
+ * Plugin Name: Materialize Video
+ * Description: Display videos as Materialize Cards. Captions will be shown as card contents if specified.
+ * Plugin URI:  https://github.com/ajatamayo/materialize-video
+ * Version:     1.0
+ * Author:      AJ Tamayo
+ * Author URI:  https://github.com/ajatamayo
+ * License:     GPL
+ * Text Domain: materialize-video
+ * Domain Path: /languages
+ *
+ */
+
+add_action( 'plugins_loaded', array( Materialize_Video::get_instance(), 'plugin_setup' ) );
+
+class Materialize_Video {
+    protected static $instance = NULL;
+    public $plugin_url = '';
+    public $plugin_path = '';
+
+    /**
+     *
+     * @since 1.0
+     */
+    public function __construct() {}
+
+    /**
+     *
+     * @since 1.0
+     */
+    public function load_language( $domain ) {
+        load_plugin_textdomain(
+            $domain,
+            FALSE,
+            $this->plugin_path . '/languages'
+        );
+    }
+
+    /**
+     *
+     * @since 1.0
+     */
+    public static function get_instance() {
+        NULL === self::$instance and self::$instance = new self;
+        return self::$instance;
+    }
+
+    /**
+     *
+     * @since 1.0
+     */
+    public function plugin_setup() {
+        $this->plugin_url    = plugins_url( '/', __FILE__ );
+        $this->plugin_path   = plugin_dir_path( __FILE__ );
+        $this->load_language( 'materialize-video' );
+
+        // Change video shortcode to use cards
+        add_filter( 'wp_video_shortcode', array( &$this, 'wrap_video_in_card' ), 10, 5 );
+
+        add_action( 'wp_enqueue_scripts', array( &$this, 'enqueue_scripts' ), 10 );
+    }
+
+    /**
+     *
+     * @since 1.0
+     */
+    function wrap_video_in_card( $output, $atts, $video, $post_id, $library ) {
+        $caption = $this->getCaptionFromGUID( $atts['mp4'] );
+
+        $classes = array( 'card' );
+        if ( !empty( $caption ) ) {
+            $classes[] = 'has-caption';
+        }
+        $classes = implode( ' ', $classes );
+
+        ob_start();
+
+        ?>
+
+        <div class="<?php echo $classes; ?>">
+            <?php echo $output; ?>
+            <?php if ( !empty( $caption ) ) : ?>
+                <div class="card-content">
+                    <p><?php echo $caption; ?></p>
+                </div>
+            <?php endif; ?>
+        </div>
+
+        <?php
+
+        return ob_get_clean();
+    }
+
+    /**
+     *
+     * @since 1.0
+     */
+    function enqueue_scripts() {
+        wp_enqueue_style( 'materialize-video', $this->plugin_url . "public/styles/video.css", array(), '1.0' );
+    }
+
+    /**
+     *
+     * @since 1.0
+     */
+    function getCaptionFromGUID( $guid ) {
+        global $wpdb;
+        return $wpdb->get_var( $wpdb->prepare( "SELECT post_excerpt FROM $wpdb->posts WHERE guid=%s", $guid ) );
+    }
+}
+
+?>
